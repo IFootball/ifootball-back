91e49cd034065f71ac73e08ac673dad7842a8022
phgiacomelli <paulohenriquegiacomelli@gmail.com>
2023-07-30 22:41:31 -0300

    Admin change player scouts done

---
+++ IFootball/src/IFootball.Application/Contracts/Documents/Responses/Player/SetPlayerScoutResponse.cs
@@ -0,0 +1,15 @@
+﻿using IFootball.Application.Contracts.Documents.Dtos;
+using System.Net;
+
+namespace IFootball.Application.Contracts.Documents.Responses.Player
+{
+    public class SetPlayerScoutResponse : BaseResponse
+    {
+        public CompletePlayerDto? Player { get; set; }
+        public SetPlayerScoutResponse(CompletePlayerDto player) 
+        {
+            Player = player;
+        }
+        public SetPlayerScoutResponse(HttpStatusCode statusCode, string? message) : base(new ErrorResponse(statusCode, message)) { }
+    }
+}
